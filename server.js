979f7d79454fb5bc26beadf6bba46a0622c55129
adam831024 <adam831024@yahoo.com.tw>
2021-04-17 17:38:25 +0800

    202104171740

--- server.js
+++ server.js
@@ -4,13 +4,27 @@ const bodyParser = require('body-parser');
 const app = express();
 const path = require("path");
 const fs = require("fs");
-// const mysql = require('mysql');
+const mysql = require('mysql');
 const mqtt = require('mqtt');
-// 456456
+
 var port = 8000; //server porrt
+
+//Database MySQL
+var con = mysql.createConnection({ host: 'localhost', user: 'adam', password: 'good1234', database: 'mes_database' });
+con.connect(function(error){
+  if(error) throw error;
+  console.log("success connect database");
+});
+
 //UI administrator
 app.get('/', function (req, res) {
   res.sendFile('UI.html', {root: __dirname});
+});
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.post('/project', function (req, res) {
+  console.log(req.body.parameter);
 });
 
 app.listen(port, function () {
