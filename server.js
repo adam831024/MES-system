219981ade44fb91f0ec3cb3e79de2db6b1365ed3
adam831024 <adam831024@yahoo.com.tw>
2021-04-19 23:15:04 +0800

    202104192315

--- server.js
+++ server.js
@@ -7,8 +7,10 @@ const fs = require("fs");
 const mysql = require('mysql');
 const mqtt = require('mqtt');
 
-var port = 8000; //server porrt
+const port = 8000; //server porrt
+const client = mqtt.connect('tcp://192.168.0.11', 1883);
 var current_date = new Date(Date.now() + 8 * 3600 * 1000);
+var n = 0; //flag
 
 //Database MySQL
 var con = mysql.createConnection({ host: 'localhost', user: 'adam', password: 'good1234', database: 'mes_database' });
@@ -24,6 +26,7 @@ app.get('/', function (req, res) {
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
+//project insert, material update
 app.post('/project', function (req, res) {
   console.log(req.body.parameter);
 
@@ -45,8 +48,6 @@ app.post('/project', function (req, res) {
 
     var sel_sql2 = "SELECT * FROM material";
     con.query(sel_sql2, function(error, result) {
-      // console.log(result[0].ingredient_A-A, result[0].ingredient_B-B, result[0].ingredient_C-C);
-
       var up_sql = `UPDATE material SET ingredient_A = '${result[0].ingredient_A - A}', ingredient_B = '${result[0].ingredient_B - B}', ingredient_C = '${result[0].ingredient_C - C}'`;
       con.query(up_sql, function(error, result){
         console.log("material update complete");
@@ -55,6 +56,29 @@ app.post('/project', function (req, res) {
   });
 });
 
+if(n){
+  app.post('/alarm', function (req, res){
+    
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    console.log(result1);
+    res.end(result2);
+
+  })
+}
+
+//MQTT
+client.on('connect', function () {
+  console.log("MQTT is connect");
+});
+
+client.subscribe('machine');
+
+client.on('message', function(topic, message){
+  console.log("topic", topic);
+  console.log("clinet receive message:", message.toString());
+});
+
+//port
 app.listen(port, function () {
   console.log('Server is listening on port: ',port);
 });
